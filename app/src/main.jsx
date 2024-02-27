1fc2f1f45dbda872b96a064c2555c0f1ba6a29ab
ehong07 <easakhong@gmail.com>
2024-02-27 15:48:04 -0800

    make background color black

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-// import './index.css'
+import './index.css'
 import '@picocss/pico'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
