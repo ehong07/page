560c3199e8e8cfc1b53ad1b309033b598173c819
ehong07 <easakhong@gmail.com>
2024-02-27 14:05:08 -0800

    add navbar

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><strong>Easak Hong</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">Test</a></li>
+        <li><button>Contact</button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
