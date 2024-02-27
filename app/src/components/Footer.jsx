9ec1fa63d3b4afcffa0ea80e8d58cccf9ce7a50e
ehong07 <easakhong@gmail.com>
2024-02-27 15:30:08 -0800

    add footer

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>Made with Bun, Pico, React, and Vite.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 // import './index.css'
 import '@picocss/pico'
 
@@ -9,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
