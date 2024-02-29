a71bcd0085df39a698e7270be5f49919e96daaf9
ehong07 <easakhong@gmail.com>
2024-02-28 16:39:58 -0800

    add links in footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -4,7 +4,17 @@ function Footer() {
   return (
     <footer>
       <div className="container">
-        <p>Made in SF with Bun, Pico, React, and Vite.</p>
+        <p>
+          Made in SF with&nbsp;
+            <a target="_blank" href="https://bun.sh/">Bun</a>
+            ,&nbsp;
+            <a target="_blank" href="https://picocss.com/">Pico</a>
+            ,&nbsp;
+            <a target="_blank" href="https://react.dev/">React</a>
+            , and&nbsp;
+            <a target="_blank" href="https://vitejs.dev/">Vite</a>
+            .
+        </p>
       </div>
     </footer>
   )
