5adf863f605f497485f59fb80210f1f6eb35a064
ehong07 <easakhong@gmail.com>
2024-02-28 16:06:04 -0800

    left align section items

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import './Footer.css';
+
 function Footer() {
   return (
     <footer>
-      <div className="container" textAlign="center">
+      <div className="container">
         <p>Made in SF with Bun, Pico, React, and Vite.</p>
       </div>
     </footer>
