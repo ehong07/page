c925c266b30cf5304af03f23d9768771590862c9
ehong07 <easakhong@gmail.com>
2024-03-07 14:44:12 -0800

    style About section

--- app/src/components/About.jsx
+++ app/src/components/About.jsx
@@ -1,21 +1,29 @@
+import './About.css';
+
 function About() {
   return (
     <div className="container">
       <h3 className="text">About</h3>
-      <p className="text">
-        Born and raised in the drastically underrated city of Cleveland in the great state of Ohio (with a 3 year stopover
-        across the globe in Singapore to defrost from Cleveland winters), I solidified my Midwestern-ness while
-        studying economics at Northwestern University before catching the coding bug and migrating west akin to an
-        IRL version of my favorite game as a child, The Oregon Trail.
-      </p>
-      <p className="text">
-        Settling in San Francisco, I embarked on a professional journey through multiple sectors of the tech industry
-        including EdTech, FinTech, and HealthTech before taking on a side quest by co-founding a lubricants consultancy.
-      </p>
-      <p className="text">
-        I’m enthusiastic about JavaScript, the React ecosystem, full-stack development, and developer experience.
-        In my free time, you can find me pondering the fleeting nature of the golf swing, moving barbells through various planes of motion, and attempting to stuff as many dumplings as humanly possible into my stomach.
-      </p>
+      <div className="text-container">
+        <p className="text">
+          Born and raised in the drastically underrated city of Cleveland in the great state of Ohio (with a 3 year stopover
+          across the globe in Singapore to defrost from Cleveland winters), I solidified my Midwestern-ness while
+          studying economics at Northwestern University before catching the coding bug and migrating west akin to an
+          IRL version of my favorite game as a child, The Oregon Trail.
+        </p>
+      </div>
+      <div className="text-container">
+        <p className="text">
+          Settling in San Francisco, I embarked on a professional journey through multiple sectors of the tech industry
+          including EdTech, FinTech, and HealthTech before taking on a side quest by co-founding a lubricants consultancy.
+        </p>
+      </div>
+      <div className="text-container">
+        <p className="text">
+          I’m enthusiastic about JavaScript, the React ecosystem, full-stack development, and developer experience.
+          In my free time, you can find me pondering the fleeting nature of the golf swing, moving barbells through various planes of motion, and attempting to stuff as many dumplings as humanly possible into my stomach.
+        </p>
+      </div>
     </div>
   );
 }
