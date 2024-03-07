b6bbe3fcf2dec412dfa224adcc789359669cba22
ehong07 <easakhong@gmail.com>
2024-03-07 13:51:37 -0800

    footer updates

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -5,7 +5,8 @@ function Footer() {
     <footer>
       <div className="container">
         <p>
-          Made in SF with&nbsp;
+          <small>
+            Copyright &copy; 2024 Easak Hong. Made in SF with&nbsp;
             <a target="_blank" href="https://bun.sh/" className="contrast">Bun</a>
             ,&nbsp;
             <a target="_blank" href="https://picocss.com/" className="contrast">Pico</a>
@@ -14,6 +15,7 @@ function Footer() {
             , and&nbsp;
             <a target="_blank" href="https://vitejs.dev/" className="contrast">Vite</a>
             .
+          </small>
         </p>
       </div>
     </footer>
